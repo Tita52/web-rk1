21421f68d8db37a1d505bd9b19c77fa9ec4fb74a
Timur Edziev <timur13edziev@gmail.com>
2024-12-08 13:39:41 +0300

    RK1

--- main.go
+++ main.go
@@ -3,89 +3,57 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 )
 
-type Input struct {
-	FirstNumber  *int    `json:"first_number"`
-	SecondNumber *int    `json:"second_number"`
-	Operator     *string `json:"operator"`
+type Response struct {
+	Result string `json:"result"`
 }
 
-type Output struct {
-	Result float64 `json:"result"`
-}
-
-// Обработчик HTTP-запроса
-func CalculateHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.WriteHeader(405)
-		w.Write([]byte("method not allowed"))
+func topKekHandler(w http.ResponseWriter, r *http.Request) {
+	numberStr := r.URL.Query().Get("number")
+	if numberStr == "" {
+		http.Error(w, "Missing 'number' query parameter", http.StatusBadRequest)
 		return
 	}
 
-	var input Input
-
-	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(&input)
+	// Преобразуем строку в целое число
+	number, err := strconv.Atoi(numberStr)
 	if err != nil {
-		w.WriteHeader(400)
-		w.Write([]byte(err.Error()))
+		http.Error(w, "Invalid 'number' parameter", http.StatusBadRequest)
 		return
 	}
 
-	if input.FirstNumber == nil {
-		w.WriteHeader(400)
-		w.Write([]byte("first_number is missing"))
-		return
-	}
-	if input.SecondNumber == nil {
-		w.WriteHeader(400)
-		w.Write([]byte("second_number is missing"))
-		return
-	}
-	if input.Operator == nil {
-		w.WriteHeader(400)
-		w.Write([]byte("operator is missing"))
-		return
+	// Определяем результат в зависимости от кратности
+	var result string
+	if number%3 == 0 && number%5 == 0 {
+		result = "TopKek"
+	} else if number%3 == 0 {
+		result = "Top"
+	} else if number%5 == 0 {
+		result = "Kek"
+	} else {
+		result = ""
 	}
 
-	var output Output
-
-	switch *input.Operator {
-	case "+":
-		output.Result = float64(*input.FirstNumber) + float64(*input.SecondNumber)
-	case "-":
-		output.Result = float64(*input.FirstNumber) - float64(*input.SecondNumber)
-	case "*":
-		output.Result = float64(*input.FirstNumber) * float64(*input.SecondNumber)
-	case "/":
-		if *input.SecondNumber == 0 {
-			w.WriteHeader(400)
-			w.Write([]byte("division by zero is not allowed"))
-			return
-		}
-		output.Result = float64(*input.FirstNumber) / float64(*input.SecondNumber)
-	default:
-		w.WriteHeader(400)
-		w.Write([]byte("unknown operator"))
-		return
+	response := Response{
+		Result: result,
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	respBytes, _ := json.Marshal(output)
-	w.Write(respBytes)
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+	}
 }
 
 func main() {
-	// Регистрируем обработчик для пути "/calculate"
-	http.HandleFunc("/calculate", CalculateHandler)
+	http.HandleFunc("/topkek", topKekHandler)
 
-	// Запускаем веб-сервер на порту 8081
-	fmt.Println("starting server...")
-	err := http.ListenAndServe("127.0.0.1:8081", nil)
-	if err != nil {
-		fmt.Println("Ошибка запуска сервера:", err)
+	fmt.Println("Server starting at http://127.0.0.1:8081")
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		log.Fatal(err)
 	}
 }
